f9bf311ad8c64e578702c5a812535abd5574d252
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-11-01 00:59:07 +0100

    Create color theme switch and add darkmode styling

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,14 +1,28 @@
 import styles from '../styles/Navbar.module.css';
+import { useState, useEffect } from 'react';
+import { HiOutlineMoon } from "react-icons/hi"
+import { BsFillSunFill } from "react-icons/bs"
 
 const Navbar = () => {
+  const [activeTheme, setActiveTheme] = useState("light");
+  const inactiveTheme = activeTheme === "light" ? "dark" : "light";
+
+  useEffect(() => {
+    document.body.dataset.theme = activeTheme;
+  }, [activeTheme]);
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <div>
           <h2>Where in the world?</h2>
         </div>
-        <div>
-          <p className={styles.paragraph}>Dark Mode</p>
+        <div onClick={() => setActiveTheme(inactiveTheme)}>
+        {activeTheme === "light" ? (
+            <p className={styles.paragraph}><HiOutlineMoon /> Dark Mode</p>
+          ) : (
+            <p className={styles.paragraph}><BsFillSunFill /> Light Mode</p>
+          )}
+          {/* <p className={styles.paragraph}>🌙 Dark Mode</p> */}
         </div>
       </nav>
     </header>
